77bfa79c07aeb76d8a04830eb4644347f500a206
Yury Prokashev <yury.prokashev@gmail.com>
2016-03-18 10:22:27 +0300

    refactor: made router independent from the order in which aggregation tasks are finished.

--- server/plotlyTracer.js
+++ server/plotlyTracer.js
@@ -1,37 +1,57 @@
 module.exports = function( wagner ) {
 
     var s = {};
-    var MyDates = wagner.invoke( function(MyDates) { return MyDates } );
-
+    var MyDates = wagner.invoke( function( MyDates ) { return MyDates } );
+    var Config = wagner.invoke( function( Config ) { return Config } );
     // gets db query results in arr of { _id, traceName }
-    // gets traceName as sting
-    // gets type of chart as string to be used for plotly.js. E.g. 'bar', 'scatter'.
+    // gets chartName as sting
     // gets monthId as string
     // outputs arr into plotly trace of { x: [ _id1, _id2..], y: [value 1, value 2], type }
-    s.makePlotlyTrace = function( traceName, arr, monthIdString ) {
+    s.makePlotlyTrace = function( chartName, arr, monthIdString, callback ) {
         // 1. Setup
         var trace = {};
         var inputTraces = Config.getChartTraces( chartName );
 
         // 2. Logic
-        if( traceName === 'dailyVolumes' ) {
-            trace = dailyVolumesTrace( arr, monthIdString );
+        // > daily Volumes CHART - 2 TRACES
+        if( chartName === 'dailyVolumes' ) {
+            if (arr[0].dailyVolumes){
+                trace = dailyVolumesTrace( inputTraces[0], arr, monthIdString );
+            }
+            else if (arr[0].monthlySpentSpeed) {
+                trace = monthlySpentSpeedTrace( inputTraces[1], arr, monthIdString );
+            }
+            else {
+                console.log('Wrong traceName in arr ' + arr[0].toString());
+            }
         }
-        else if ( traceName === 'monthlySpentSpeed' ){
-            trace = monthlySpentSpeedTrace( arr, monthIdString );
+
+        // > categoryVolumes CHART - 1 TRACE
+        else if ( chartName === 'categoryVolumes' ) {
+            if (arr[0].categoryVolume){
+                trace = volumesByCategoryTrace( inputTraces[0], arr, monthIdString );
+            }
+            else {
+                console.log('Wrong traceName in arr ' + arr[0].toString());
+            }
         }
-        else if ( traceName === 'volumesByCategory' ){
-            trace = volumesByCategoryTrace( arr, monthIdString );
+
+        // > expenseFrequency CHART - 1 TRACE
+        else if (chartName === 'expenseFrequency' ) {
+            if (arr[0].expenseFrequency) {
+                trace = expenseFrequencyTrace( inputTraces[0], arr, monthIdString );
+            }
+            else {
+                console.log('Wrong traceName in arr ' + arr[0].toString());
+            }
         }
-        else if (traceName === 'expenseFrequency' ){
-            trace = expenseFrequencyTrace( arr, monthIdString );
-        }
+
         else {
-            console.log('invalid traceName');
+            console.log('invalid chartName or traceName: ' + chartName + '-' + arr.toString());
         }
 
         // 3. Result
-        return trace;
+        callback( trace );
     };
 
     // helper functions
@@ -53,7 +73,7 @@ module.exports = function( wagner ) {
         };
 
         // 3. Return result
-        return trace;
+        return inputTrace;
     };
 
     var monthlySpentSpeedTrace = function( inputTrace, arr, monthIdString ){
@@ -71,7 +91,6 @@ module.exports = function( wagner ) {
 
         // 3. Return result
         return inputTrace;
-        // myEmitter.emit('dailyVolumesPlotlyTraceReady', inputTrace);
     };
 
     var volumesByCategoryTrace = function( inputTrace, arr, monthIdString ){
@@ -80,8 +99,6 @@ module.exports = function( wagner ) {
         inputTrace.values = [];
 
         // 2. Logic
-        //[{"_id":1,"categoryVolume":900},{"_id":2,"categoryVolume":90}]
-
         for( var i = 0; i < arr.length; i++ ) {
 
             //console.log(arr);
@@ -89,8 +106,6 @@ module.exports = function( wagner ) {
             //console.log(inputTrace.labels[i]);
             inputTrace.values[i] = arr[i].categoryVolume;
         }
-
-        //console.log(inputTrace);
 
         // 3. Return result
         return inputTrace;
@@ -108,7 +123,6 @@ module.exports = function( wagner ) {
 
         // 3. Return result
         return inputTrace;
-        // myEmitter.emit('expenseFrequencyPlotlyTraceReady', inputTrace);
     };
 
     // register Service in wagner
